5f07d6ca5daed48e6d1be5b6f064ddae92c15eab
Renko Brauckman <renkob2005@gmail.com>
2024-10-19 16:25:06 +0200

    start

---
+++ C#_basics/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello this is my C# basics folder");
+    }
+}
---
+++ C#_basics/getters_setters.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Getters_Setters{
+    public void Main()
+    {
+        Console.Write("in this file i made a student class with some");
+    }
+}
